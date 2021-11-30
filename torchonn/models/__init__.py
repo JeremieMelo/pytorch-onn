c6ea5b5ece7a9b95c1486a1c5de228777f548cfb
JeremieMelo <jqgu1996@163.com>
2021-11-29 20:27:23 -0600

    [v0.0.2] update fftonn support

--- torchonn/models/__init__.py
+++ torchonn/models/__init__.py
@@ -1,6 +1,13 @@
+'''
+Description:
+Author: Jiaqi Gu (jqgu@utexas.edu)
+Date: 2021-11-29 20:25:40
+LastEditors: Jiaqi Gu (jqgu@utexas.edu)
+LastEditTime: 2021-11-29 20:25:40
+'''
 import importlib
 import os
-from .base_model import *
+# from .base_model import *
 
 # automatically import any Python files in this directory
 for file in sorted(os.listdir(os.path.dirname(__file__))):

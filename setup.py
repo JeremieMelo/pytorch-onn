357f0e84348eaed173faf4cb8acb8948fb153c5a
JeremieMelo <jqgu1996@163.com>
2021-11-29 20:04:27 -0600

    [v0.0.2] update fftonn support

--- setup.py
+++ setup.py
@@ -172,8 +172,9 @@ setup(
     ],
     python_requires=">=3.6",
     include_package_data=True,
-    packages=["models", "layers", "op", "devices"],
-    package_dir={"": "torchonn"},
+    packages=find_packages(),
+    # packages=["models", "layers", "op", "devices"],
+    # package_dir={"": "torchonn"},
     ext_modules=ext_modules,
     cmdclass={"build_ext": BuildExtension, "clean": CleanCommand},
 )
